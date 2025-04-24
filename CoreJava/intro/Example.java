15e3c826a7510edbcef002f52b8598e05d3663a1
DLR2204 <n130488@rguktn.ac.in>
2025-04-24 08:55:06 +0530

    Add files via upload

---
+++ CoreJava/intro/Example.java
@@ -0,0 +1,13 @@
+package intro;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		byte value = 45; // initialization of a variable
+
+		System.out.println(value);
+
+	}
+
+}
