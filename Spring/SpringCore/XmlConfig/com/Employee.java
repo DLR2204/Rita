f13578a94a541367a7b1c860ffd46565da924550
DLR2204 <n130488@rguktn.ac.in>
2025-06-13 22:07:38 +0530

    Add files via upload

---
+++ Spring/SpringCore/XmlConfig/com/Employee.java
@@ -0,0 +1,63 @@
+package com;
+
+public class Employee {
+
+	private String empId;
+	
+	private String empName;
+	
+	private int empSal;
+	
+	private String empAdd;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpSal() {
+		return empSal;
+	}
+
+	public void setEmpSal(int empSal) {
+		this.empSal = empSal;
+	}
+
+	public String getEmpAdd() {
+		return empAdd;
+	}
+
+	public void setEmpAdd(String empAdd) {
+		this.empAdd = empAdd;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", empAdd=" + empAdd + "]";
+	}
+
+	public Employee(String empId, String empName, int empSal, String empAdd) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+		this.empAdd = empAdd;
+	}
+	
+	
+}
